2c49427e6d92eca36fde809823227baafa6111ca
dmiweb <dimbons@mail.ru>
2025-02-01 18:36:18 +0300

    created the project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from './App.tsx';
+import './index.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </StrictMode>,
 )
---
+++ src/redux/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "../slices/moviesSlice";
+
+export const store = configureStore({
+  reducer: {
+    movies: moviesReducer
+  },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
